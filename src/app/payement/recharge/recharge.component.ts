48192cb661ac2aa557f92df33c57e3ee9e1d03f5
elfailali <el.failali.lbalghiti@gmail.com>
2023-05-29 05:48:03 +0200

    Add data to json file

--- src/app/payement/recharge/recharge.component.ts
+++ src/app/payement/recharge/recharge.component.ts
@@ -1,11 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Provider } from '../models/provider.model';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-
-
-
-
+import { SharedDataService } from '../shared-data.service';
 
 
 @Component({
@@ -13,10 +9,14 @@ import { RouterModule } from '@angular/router';
   templateUrl: './recharge.component.html',
   styleUrls: ['./recharge.component.css']
 })
-export class RechargeComponent {
-
-  provider?: Provider =  new Provider(4, 'IAM', './../../assets/logos/maroc-telecom-blanc-ar-grande.jpg', 'Recharge', 'Maroc Telecom'); //FOR TEST
-  // surname?: string;
+export class RechargeComponent implements OnInit {
+  provider?: Provider = new Provider(
+    4,
+    'IAM',
+    './../../assets/logos/maroc-telecom-blanc-ar-grande.jpg',
+    'Recharge',
+    'Maroc Telecom'
+  );
 
   phone?: string;
   creancierCode?: string;
@@ -26,129 +26,46 @@ export class RechargeComponent {
   loading = false;
   submitted = false;
 
-  
-  // submitted1 = false;
+  rechargeForm!: FormGroup;
 
-  // closeResult?: string;
+  constructor(private formBuilder: FormBuilder, private sharedDataService: SharedDataService) {}
 
-  // otpError: boolean = false;
+  ngOnInit(): void {
+    this.rechargeForm = this.formBuilder.group({
+      phoneNumber: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+      creancierCode: ['', Validators.required],
+      amount: ['10', Validators.required]
+    });
+    console.log("amount")
+    console.log(this.amount)
+  }
 
+  onSubmit(): void {
+    this.submitted = true;
+    console.log('hhhhhhhhhhhhhhhhhhhhhhhhhh')
 
-  // constructor(private router: Router,
-  //   private route: ActivatedRoute,
-  //   private providerService: ProviderService,
-  //   private alertService: AlertService,
-  //   private modalService: NgbModal,
-  //   private tokenStorageService: TokenStorageService) { }
+    if (this.rechargeForm.invalid) {
+      console.log('sorti mr')
+      return;
+    }
 
-  // ngOnInit(): void {
-  //   this.surname = this.route.snapshot.params['surname'];
-  //   this.provider = this.providerService.findBySurname(this.surname!, "recharge");
-  //   if (!this.provider) {
-  //     this.alertService.warn("Introuvable", { keepAfterRouteChange: true });
-  //     this.router.navigate(['../../'], { relativeTo: this.route });
-  //   }
-  // }
+    this.date = new Date().toLocaleDateString();
+    this.phone = this.rechargeForm.get('phoneNumber')?.value;
+    this.creancierCode = this.rechargeForm.get('creancierCode')?.value;
+    this.amount = this.rechargeForm.get('amount')?.value;
 
-  // rechargeForm = new FormGroup({
-  //   phoneNumber: new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]),
-  //   creancierCode: new FormControl('', Validators.required),
-  //   amount: new FormControl('', Validators.required),
-  // });
+    console.log(this.amount)
 
-  // otpForm = new FormGroup({
-  //   otp: new FormControl('', Validators.required),
-  // });
-
-
-  // get f() { return this.rechargeForm.controls; }
-  // get o() { return this.otpForm.controls; }
-
-
-  // onSubmit() {
-
-  //   this.providerService.recharge(this.rechargeForm.value).subscribe(
-  //     {
-  //       next: (v: any) => {
-  //         this.loading = false;
-  //       },
-  //       error: (e: any) => {
-  //         this.alertService.error(e.statusText);
-  //         this.loading = false;
-  //       },
-  //       complete: () => {
-  //         this.modalService.dismissAll();
-  //         this.alertService.success("Operation effectuée avec succés");
-  //       }
-  //     }
-  //   );
+    // Store the data in the shared service
+    this.sharedDataService.setRechargeData({
+      phoneNumber: this.phone,
+      creancierCode: this.creancierCode,
+      amount: this.amount
+    });
 
 
-  // }
 
-  // openDialog(content: any) {
-  //   this.submitted = true;
-  //   this.alertService.clear();
-  //   if (this.rechargeForm.invalid) {
-  //     return;
-  //   }
-  //   this.date = (new Date()).toLocaleDateString();
-  //   this.phone = this.f['phoneNumber'].value;
-  //   this.creancierCode = this.f['creancierCode'].value;
-  //   this.amount = this.f['amount'].value;
-  //   this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
-  //     this.closeResult = `Closed with: ${result}`;
-  //   }, (reason) => {
-  //     this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-  //   });
-  // }
-
-  // openOTPDialog(content: any) {
-  //   this.providerService.sendOTP(this.tokenStorageService.getPhoneNumber()).subscribe({
-  //     next: (v: any) => {
-  //     },
-  //     error: (e: any) => {
-  //       console.log(e)
-  //     },
-  //     complete: () => {
-  //     }
-  //   });
-  //   this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
-  //     this.closeResult = `Closed with: ${result}`;
-  //   }, (reason) => {
-  //     this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-  //   });
-  // }
-
-  // verifyOTP() {
-  //   this.submitted1 = true;
-  //   if (this.otpForm.invalid) {
-  //     return;
-  //   }
-  //   this.providerService.verifyOTP({ ...this.otpForm.value, phone: this.rechargeForm.value.phoneNumber }).subscribe(
-  //     {
-  //       next: (v: any) => {
-  //         this.otpError = false;
-  //       },
-  //       error: (e: any) => {
-  //         this.otpError = true;
-  //       },
-  //       complete: () => {
-  //         this.onSubmit();
-  //       }
-  //     }
-  //   );
-  // }
-
-
-  // private getDismissReason(reason: any): string {
-  //   if (reason === ModalDismissReasons.ESC) {
-  //     return 'by pressing ESC';
-  //   } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-  //     return 'by clicking on a backdrop';
-  //   } else {
-  //     return `with: ${reason}`;
-  //   }
-  // }
-
+    // Perform further actions or submit the form data
+    // ...
+  }
 }
